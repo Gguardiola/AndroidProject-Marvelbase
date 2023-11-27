d56bf949ceba776b7183f7f548f0564e175eea85
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2023-11-27 16:12:56 +0100

    Added the Main Page activity and design imported from Figma.

--- app/src/main/java/com/appengers/marvelbase/MainActivity.java
+++ app/src/main/java/com/appengers/marvelbase/MainActivity.java
@@ -3,12 +3,28 @@ package com.appengers.marvelbase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton btnCharacters, btnComics, btnFavorites, btnCreators;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page_activity);
+
+        btnCreators = (ImageButton) findViewById(R.id.creators_btn);
+        btnCharacters = (ImageButton) findViewById(R.id.characters_btn);
+        btnComics = (ImageButton) findViewById(R.id.comics_btn);
+        btnFavorites = (ImageButton) findViewById(R.id.favorites_btn);
+
+        btnCreators.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Creator test", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/appengers/marvelbase/Models/Favorites.java
+++
@@ -1,6 +0,0 @@
-package com.appengers.marvelbase.Models;
-
-public class Favorites {
-    //Favorites: Structure of the Favorite objects received from Firebase BD
-
-}
