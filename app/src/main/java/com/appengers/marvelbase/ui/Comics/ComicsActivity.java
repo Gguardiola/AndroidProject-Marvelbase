bca8d2ccee2abe1f8660f4883b53af6fac938547
Aklix2606 <alexandre.picas@estudiantat.upc.edu>
2023-12-05 12:15:18 +0100

    Add Comics adapter and Recycler classes + layout

--- app/src/main/java/com/appengers/marvelbase/ui/Comics/ComicsActivity.java
+++ app/src/main/java/com/appengers/marvelbase/ui/Comics/ComicsActivity.java
@@ -1,7 +1,23 @@
 package com.appengers.marvelbase.ui.Comics;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.appengers.marvelbase.Models.Comics;
+import com.appengers.marvelbase.R;
+
+import java.util.ArrayList;
 
 public class ComicsActivity extends AppCompatActivity {
+    RecyclerView listofComics;
+    ArrayList<Comics> lComics;
 
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_comic);
+
+        listofComics = (RecyclerView) findViewById(R.id.recyChar);
+    }
 }
